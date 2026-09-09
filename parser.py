a1284f79d364e6da0c3b4c39331bc78141dcc294
root <root@A9TT82SQ.localdomain>
2026-09-09 23:05:34 +0200

    upd 4

--- parser.py
+++ parser.py
@@ -1,23 +1,29 @@
-
+from tools import Drone, Zone, Data
 
 def map_path() -> str:
-    return ""
+    return "" # path will be added later
 
 
-def parser_txt() -> None:
+def parser_txt() -> Data:
     try:
         with open(map_path(), "r", encoding="utf-8") as txt_file:
-            drone
+            data = Data()
             for line in txt_file:
                 if line.startswith("#"):
                     continue
                 elif line.startswith("nb_drones"):
+                    lst_drone = [Drone(i, [0, 0]) for i in range()]
                 elif line.startswith("start_hub"):
+                    data.start = Zone()
                 elif line.startswith("end_hub"):
+                    data.start = Zone()
                 elif line.startswith("hub"):
+                    lst_zone = Zone()
                 elif line.startswith("connection"):
+
                 else:
                     print(f"line not good:\n'{line}'\n")
-
-    except Exception:
-        raise Exception("Cant open file txt ")
+        data.drones = tuple(lst_drone)
+        data.zones = tuple(lst_zone)
+    except Exception as e:
+        raise Exception(f"Cant open file txt: {e}")
---
+++ tools.py
@@ -0,0 +1,75 @@
+from enum import Enum
+from typing import Optional
+from pydantic import BaseModel, Field, ValidationError, model_validator
+
+
+MAX_DRONES_SIZE = 400
+
+
+class colors(str, Enum):
+    red = "red"
+    orange = "orange"
+    yellow = "yellow"
+    green = "green"
+    blue = "blue"
+    purple = "purple"
+    pink = "pink"
+    brown = "brown"
+    black = "black"
+    white = "white"
+    gray = "gray"
+
+
+class Drone(BaseModel):
+    id: int
+    location: list[int]
+
+
+class Zone(BaseModel):
+    name: str
+    coord: tuple[int, int]
+    color: colors
+    status: Optional[str]
+    links: Optional[tuple[str, str]]
+    max_drones: Optional[int] = Field(ge=1, le=MAX_DRONES_SIZE)
+
+    @model_validator(mode="after")
+    def validate_zone_rules(self):
+        if self.status not in ("restricted", "normal", "blocked", "priority"):
+            raise ValidationError("status is not correct")
+        elif self.status is None:
+            self.status = "normal"
+        if self.status is None:
+            self.max_drones = 1
+
+
+class Zone_queue(BaseModel):
+    drones_waiting_nbr: int
+    drones_lst: list[Drone]
+    max_link_capacity: int
+    zone_location: tuple[str, str]
+
+
+class Data(BaseModel):
+    start: Zone
+    end: Zone
+    drones: int
+    zones: tuple[Zone, ...]
+    zone_queues: tuple[Zone_queue, ...]
+
+    @model_validator(mode="after")
+    def validate_rules(self):
+        print("testing")
+
+    def show_links(self):
+        for zone in self.zones:
+            print(f"{zone.links if zone.links is not None else ''}\n")
+
+    def show_queue(self, zone_1: Zone, zone_2: Zone):
+        for queue in self.zone_queues:
+            if queue.zone_location == (zone_1, zone_2):
+                print(f"\tzone location = {queue.zone_location}",
+                      f"\t\tdrones lst = {queue.drones_lst}",
+                      f"\t\tmax link capacity = {queue.max_link_capacity}",
+                      f"\t\tzone location = {queue.zone_location}",
+                      f"\t\tdrones waiting nbr = {queue.drones_waiting_nbr}")
